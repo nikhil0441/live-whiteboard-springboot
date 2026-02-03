83a572007cc4bf991dadf8c149b053e9b48676f2
nikhil0441 <nikhilpatel03022004@gmail.com>
2026-02-03 22:16:47 +0530

    first commit

---
+++ src/main/java/com/example/livewhiteBoard/LiveWhiteboardSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.livewhiteBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveWhiteboardSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveWhiteboardSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/livewhiteBoard/LiveWhiteboardSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livewhiteBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveWhiteboardSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
