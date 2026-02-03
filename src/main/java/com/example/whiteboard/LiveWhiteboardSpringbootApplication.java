6b75e65bcad303f577f3f74206fbec5c343c5181
nikhil0441 <nikhilpatel03022004@gmail.com>
2026-02-03 22:19:45 +0530

    Project structure created

--- src/main/java/com/example/whiteboard/LiveWhiteboardSpringbootApplication.java
+++ src/main/java/com/example/whiteboard/LiveWhiteboardSpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.example.livewhiteBoard;
+package com.example.whiteboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/whiteboard/LiveWhiteboardSpringbootApplicationTests.java
+++ src/test/java/com/example/whiteboard/LiveWhiteboardSpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.livewhiteBoard;
+package com.example.whiteboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
